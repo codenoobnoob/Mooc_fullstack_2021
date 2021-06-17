ec3acb8a11b5ea295b6514c7051aa939c65f5194
Patrik Hellström <patrikmhellstrom@gmail.com>
2021-06-17 14:08:00 +0300

    maatiedot updated

--- osa2/maatiedot/src/App.js
+++ osa2/maatiedot/src/App.js
@@ -1,6 +1,106 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
+const ShowCountry = ({
+  openCountry,
+  CloseCountry,
+  countries,
+  weatherData,
+  setWeatherData,
+}) => {
+  if (openCountry === "") {
+    return null;
+  } else {
+    var country = undefined;
+    countries.forEach((item) => {
+      if (item.name === openCountry) {
+        country = item;
+        const params = {
+          access_key: process.env.REACT_APP_API_KEY,
+          query: country.capital,
+        };
+        if (weatherData.city !== country.capital) {
+          axios
+            .get("http://api.weatherstack.com/current", { params })
+            .then((response) => {
+              setWeatherData({
+                city: response.data.location.name,
+                temp: response.data.current.temperature,
+                img: response.data.current.weather_icons,
+                windSpeed: response.data.current.wind_speed,
+                windDirection: response.data.current.wind_dir,
+              });
+              console.log(weatherData);
+            })
+            .catch((error) => {
+              console.log(error);
+            });
+        }
+      }
+    });
+
+    return (
+      <div>
+        <h2>{country.name}</h2>
+        <p>Capital: {country.capital}</p>
+        <p>Population: {country.population}</p>
+        <h3>Languages</h3>
+        <ul>
+          {country.languages.map((language) => (
+            <li key={language.name}>{language.name}</li>
+          ))}
+        </ul>
+        <p>
+          <img src={country.flag} alt="Flag" height="10%" width="10%"></img>
+        </p>
+        <h3>Weather in {country.capital}</h3>
+        <p>
+          <b>temperature: </b>
+          {weatherData.temp} Celcius
+        </p>
+        <p>
+          <img
+            src={weatherData.img[0]}
+            alt="Weather icon"
+            height="5%"
+            width="5%"
+          ></img>
+        </p>
+        <p>
+          <b>wind: </b>
+          {weatherData.windSpeed} km/h, direction {weatherData.windDirection}
+        </p>
+        <button onClick={CloseCountry}>close</button>
+      </div>
+    );
+  }
+};
+
+const Countries = ({ countries, filter, openCountry, OpenCountry }) => {
+  const showlist = [];
+  countries.forEach((country) => {
+    if (country.name.toLowerCase().includes(filter.toLowerCase())) {
+      showlist.push(
+        <div key={country.name}>
+          {country.name}
+          <button value={country.name} onClick={OpenCountry}>
+            show
+          </button>
+        </div>
+      );
+    }
+  });
+  if (showlist.length > 10 || showlist.length === 0) {
+    return <p>Too many matches, specify another filter</p>;
+  } else {
+    if (openCountry === "") {
+      return showlist;
+    } else {
+      return null;
+    }
+  }
+};
+
 const App = () => {
   const [findCountry, setFindCountry] = useState("");
   const [countries, setCountries] = useState([]);
@@ -35,100 +135,6 @@ const App = () => {
     setOpenCountry("");
   };
 
-  const ShowCountry = () => {
-    if (openCountry === "") {
-      return null;
-    } else {
-      var country = undefined;
-      countries.forEach((item) => {
-        if (item.name === openCountry) {
-          country = item;
-          const params = {
-            access_key: process.env.REACT_APP_API_KEY,
-            query: country.capital,
-          };
-          if (weatherData.city !== country.capital) {
-            axios
-              .get("http://api.weatherstack.com/current", { params })
-              .then((response) => {
-                setWeatherData({
-                  city: response.data.location.name,
-                  temp: response.data.current.temperature,
-                  img: response.data.current.weather_icons,
-                  windSpeed: response.data.current.wind_speed,
-                  windDirection: response.data.current.wind_dir,
-                });
-                console.log(weatherData);
-              })
-              .catch((error) => {
-                console.log(error);
-              });
-          }
-        }
-      });
-
-      return (
-        <div>
-          <h2>{country.name}</h2>
-          <p>Capital: {country.capital}</p>
-          <p>Population: {country.population}</p>
-          <h3>Languages</h3>
-          <ul>
-            {country.languages.map((language) => (
-              <li key={language.name}>{language.name}</li>
-            ))}
-          </ul>
-          <p>
-            <img src={country.flag} alt="Flag" height="10%" width="10%"></img>
-          </p>
-          <h3>Weather in {country.capital}</h3>
-          <p>
-            <b>temperature: </b>
-            {weatherData.temp} Celcius
-          </p>
-          <p>
-            <img
-              src={weatherData.img[0]}
-              alt="Weather icon"
-              height="5%"
-              width="5%"
-            ></img>
-          </p>
-          <p>
-            <b>wind: </b>
-            {weatherData.windSpeed} km/h, direction {weatherData.windDirection}
-          </p>
-          <button onClick={CloseCountry}>close</button>
-        </div>
-      );
-    }
-  };
-
-  const Countries = ({ countries, filter }) => {
-    const showlist = [];
-    countries.forEach((country) => {
-      if (country.name.toLowerCase().includes(filter.toLowerCase())) {
-        showlist.push(
-          <div key={country.name}>
-            {country.name}
-            <button value={country.name} onClick={OpenCountry}>
-              show
-            </button>
-          </div>
-        );
-      }
-    });
-    if (showlist.length > 10 || showlist.length === 0) {
-      return <p>Too many matches, specify another filter</p>;
-    } else {
-      if (openCountry === "") {
-        return showlist;
-      } else {
-        return null;
-      }
-    }
-  };
-
   return (
     <div>
       <p>Find countries</p>
@@ -137,8 +143,21 @@ const App = () => {
         value={findCountry}
         onChange={onFindCountryChange}
       />
-      <ShowCountry key="openCountry" />
-      <Countries key="countryList" countries={countries} filter={findCountry} />
+      <ShowCountry
+        key="openCountry"
+        openCountry={openCountry}
+        CloseCountry={CloseCountry}
+        countries={countries}
+        weatherData={weatherData}
+        setWeatherData={setWeatherData}
+      />
+      <Countries
+        key="countryList"
+        countries={countries}
+        filter={findCountry}
+        openCountry={openCountry}
+        OpenCountry={OpenCountry}
+      />
     </div>
   );
 };
