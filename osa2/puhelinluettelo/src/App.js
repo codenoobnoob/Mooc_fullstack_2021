b23f19f8659af2a375396f6ffba95ae473776e88
Patrik Hellström <patrikmhellstrom@gmail.com>
2021-06-18 16:25:39 +0300

    updated part3

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -2,70 +2,97 @@ import React, { useState, useEffect } from "react";
 import Persons from "./components/Render";
 import Filter from "./components/Filter";
 import PersonForm from "./components/PersonForm";
-import notifications from "./components/Notifications"
-import personsService from "./services/persons"
+import notifications from "./components/Notifications";
+import personsService from "./services/persons";
 
 const App = () => {
   const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [filter, setFilter] = useState("");
-  const [message, setMessage] = useState({message: null, isError: false});
+  const [message, setMessage] = useState({ message: null, isError: false });
 
   const getDevData = () => {
-    personsService.getData().then(initialData => {
-      setPersons(initialData)
-    }).catch(error => {
-      notifications.errorMessage(`Could not connect to the server. Please check server connectivity and refresh the page! ${error}`, true, setMessage)
-    })
-  }
-
+    personsService
+      .getData()
+      .then((initialData) => {
+        setPersons(initialData);
+      })
+      .catch((error) => {
+        notifications.errorMessage(
+          `Could not connect to the server. Please check server connectivity and refresh the page! ${error}`,
+          true,
+          setMessage
+        );
+      });
+  };
 
   useEffect(getDevData, []);
-  
- 
 
   const AddName = (e) => {
     e.preventDefault();
-    let newMessage = null
-    let newIsError = false
-    const indexOfExisting = persons.map((person) => person.name).indexOf(newName) 
+    let newMessage = null;
+    let newIsError = false;
+    const indexOfExisting = persons
+      .map((person) => person.name)
+      .indexOf(newName);
     if (indexOfExisting !== -1) {
-      if (window.confirm(`${newName} is aleady added to phonebook, replace the old number with the new one?`)) {
-        newMessage = `${newName} phone number was changed`
-        const oldId = persons[indexOfExisting].id
+      if (
+        window.confirm(
+          `${newName} is aleady added to phonebook, replace the old number with the new one?`
+        )
+      ) {
+        newMessage = `${newName} phone number was changed`;
+        const oldId = persons[indexOfExisting].id;
         const updatedPerson = {
           name: newName,
           number: newNumber,
-          id: oldId
-        }
-        personsService.updateData(persons[indexOfExisting].id, updatedPerson).then(returnedPerson => setPersons(persons.map(person => person.id !== oldId ? person : returnedPerson))).catch(error => {
-          newMessage = "error while trying to update person"
-          newIsError = true
-        })
+          id: oldId,
+        };
+        personsService
+          .updateData(persons[indexOfExisting].id, updatedPerson)
+          .then((returnedPerson) =>
+            setPersons(
+              persons.map((person) =>
+                person.id !== oldId ? person : returnedPerson
+              )
+            )
+          )
+          .catch((error) => {
+            newMessage = "error while trying to update person";
+            newIsError = true;
+          });
       }
     } else {
-      newMessage = `${newName} added to phonebook`
+      newMessage = `${newName} added to phonebook`;
       const newPerson = {
         name: newName,
         number: newNumber,
-        id: Math.max(persons.map((person) => person.id) + 1),
       };
-      setPersons(
-        persons.concat({
-          newPerson,
+      // setPersons(
+      //   persons.concat({
+      //     newPerson,
+      //   })
+      // );
+      personsService
+        .addData(newPerson)
+        .then((returnedPerson) => {
+          console.log(returnedPerson);
+          setPersons(persons.concat(returnedPerson));
         })
-      );
-      personsService.addData(newPerson).then(returnedPerson => {
-        setPersons(persons.concat(returnedPerson));
-      }).catch(error => {
-        newMessage = "error while trying to add person"
-        newIsError = true
-      })
+        .catch((error) => {
+          newMessage = "error while trying to add person";
+          newIsError = true;
+        });
     }
     setNewName("");
     setNewNumber("");
-    notifications.setMessageWithTimeout(newMessage, newIsError, setMessage, 5000)
+    notifications.setMessageWithTimeout(
+      newMessage,
+      newIsError,
+      setMessage,
+      5000
+    );
   };
   const handleNameChange = (e) => {
     e.preventDefault();
@@ -81,25 +108,53 @@ const App = () => {
   };
 
   const deletePerson = (e) => {
-    e.preventDefault()
-    if (window.confirm(`Are you sure you want to delete ${e.target.dataset.fullname}`)) {
-      personsService.removeData(e.target.id).then((response) => { 
-        if (response) {
-          setPersons(persons.filter(person => person.id !== parseInt(e.target.id, 10)))
-          notifications.setMessageWithTimeout(`Person ${e.target.dataset.fullname} was removed from the server`, false, setMessage, 5000)
-        } 
-      }).catch(error => {
-        notifications.setMessageWithTimeout(`Person ${e.target.dataset.fullname} was already removed from the server, Please refresh the page.`, true, setMessage, 5000)
-      })
+    e.preventDefault();
+    if (
+      window.confirm(
+        `Are you sure you want to delete ${e.target.dataset.fullname}`
+      )
+    ) {
+      personsService
+        .removeData(e.target.id)
+        .then((response) => {
+          if (response) {
+            setPersons(
+              persons.filter(
+                (person) => person.id !== parseInt(e.target.id, 10)
+              )
+            );
+            notifications.setMessageWithTimeout(
+              `Person ${e.target.dataset.fullname} was removed from the server`,
+              false,
+              setMessage,
+              5000
+            );
+          }
+        })
+        .catch((error) => {
+          notifications.setMessageWithTimeout(
+            `Person ${e.target.dataset.fullname} was already removed from the server, Please refresh the page.`,
+            true,
+            setMessage,
+            5000
+          );
+        });
     }
-
-  }
+  };
 
   return (
     <div className="mainWindow">
       <h1>Phonebook</h1>
-      <notifications.NotificationMessage key="messages" message={message.message} isError={message.isError} />
-      <Filter key="filter" filter={filter} handeleFilterChange={handeleFilterChange} />
+      <notifications.NotificationMessage
+        key="messages"
+        message={message.message}
+        isError={message.isError}
+      />
+      <Filter
+        key="filter"
+        filter={filter}
+        handeleFilterChange={handeleFilterChange}
+      />
       <PersonForm
         key="personform"
         newName={newName}
@@ -109,8 +164,13 @@ const App = () => {
         addName={AddName}
       />
       <h2>Numbers</h2>
-      <Persons key="persons" persons={persons} filter={filter} deletePerson={deletePerson} />
-    </div>  
+      <Persons
+        key="persons"
+        persons={persons}
+        filter={filter}
+        deletePerson={deletePerson}
+      />
+    </div>
   );
 };
 
--- osa2/puhelinluettelo/src/services/persons.js
+++ osa2/puhelinluettelo/src/services/persons.js
@@ -1,10 +1,13 @@
 import axios from "axios";
-const baseUrl = "http://localhost:3001/persons";
+const baseUrl = "/api/persons";
 
 const getData = () => axios.get(baseUrl).then((response) => response.data);
 
 const addData = (newData) =>
-  axios.post(baseUrl, newData).then((response) => response.data);
+  axios.post(baseUrl, newData).then((response) => {
+    console.log(response.data);
+    return response.data;
+  });
 
 const updateData = (id, newData) =>
   axios.put(`${baseUrl}/${id}`, newData).then((response) => response.data);
